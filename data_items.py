836a678d8c2faf9066f651f476aff285bafdc8ec
Kaycee Nalzaro <kaycee.nalzaro@g.msuiit.edu.ph>
2025-12-30 18:54:36 +0800

    First Commit

---
+++ data_items.py
@@ -0,0 +1,31 @@
+# Fixed vocabulary of drainage conditions (18 items)
+
+TRASH_ITEMS = [
+    "no_trash",
+    "light_trash",
+    "heavy_trash",
+    "plastic_trash",
+    "organic_trash",
+    "mixed_trash"
+]
+
+FLOW_ITEMS = [
+    "fast_flow",
+    "normal_flow",
+    "slow_flow",
+    "stagnant_flow",
+    "backflow"
+]
+
+VISUAL_ITEMS = [
+    "clear_water",
+    "dark_water",
+    "foam_present",
+    "debris_spread"
+]
+
+STATE_ITEMS = [
+    "clean",
+    "partially_clogged",
+    "clogged"
+]
